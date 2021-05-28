0a5222638f905d64426419244c60f64a46fd0210
admin <admin@YD02-19>
2021-05-28 12:50:46 +0900

    commit

--- src/com/pet/board/vo/BoardVO.java
+++ src/com/pet/board/vo/BoardVO.java
@@ -1,5 +1,61 @@
 package com.pet.board.vo;
 
+import java.util.Date;
+
 public class BoardVO {
 
+	private int boardid;
+	private String title;
+	private String content;
+	private String writer;
+	private Date date;
+	private int hit;
+	private int like;
+	
+	
+	public int getBoardid() {
+		return boardid;
+	}
+	public void setBoardid(int boardid) {
+		this.boardid = boardid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	
+	
+	
 }
--- src/com/pet/member/vo/MemberVO.java
+++ src/com/pet/member/vo/MemberVO.java
@@ -2,4 +2,42 @@ package com.pet.member.vo;
 
 public class MemberVO {
 
+	private String id;
+	private String name;
+	private String password;
+	private String phoneNB;
+	private String address;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNB() {
+		return phoneNB;
+	}
+	public void setPhoneNB(String phoneNB) {
+		this.phoneNB = phoneNB;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
 }
