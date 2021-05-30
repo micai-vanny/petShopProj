35827ba79b6a167126f963cc950980683430ea3a
Banya <hello_vanny@naver.com>
2021-05-30 16:52:08 +0900

    20210530 DAO 수정 : 반야

--- petShopProj/src/com/pet/common/DAO.java
+++ petShopProj/src/com/pet/common/DAO.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DAO {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
-	private String url = "jdbc:oracle:thin:petshopproj.ccdbghqtd8mv.ap-northeast-2.rds.amazonaws.com:1521:xe";
+	private String url = "jdbc:oracle:thin:petshopproj.ccdbghqtd8mv.ap-northeast-2.rds.amazonaws.com:1521:petorcl";
 	private String user = "petshop";
 	private String passwd = "petshop01";
 	public Connection conn;
