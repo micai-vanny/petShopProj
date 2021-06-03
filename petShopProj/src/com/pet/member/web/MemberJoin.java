206ac9ee69bb21baa8c8b48b7ca97d52e4c1877f
admin <admin@YD02-19>
2021-06-03 16:44:59 +0900

    주소 api 적용

--- petShopProj/src/com/pet/member/web/MemberJoin.java
+++ petShopProj/src/com/pet/member/web/MemberJoin.java
@@ -16,13 +16,19 @@ public class MemberJoin implements DbCommand {
 		String id = request.getParameter("memberId");
 		String pwd = request.getParameter("password");
 		String name = request.getParameter("name");
-		String adr = request.getParameter("adr");
 		String email = request.getParameter("email");
+		
+		String adr1 = request.getParameter("postcode");
+		String adr2 = request.getParameter("roadAddress");
+		String adr3 = request.getParameter("detailAddress");
+		String adr4 = request.getParameter("extraAddress");
+		
+		String totaladr = "(" + adr1 + ")" +adr2+adr3+adr4;
 		
 		MemberVO vo = new MemberVO();
 		vo.setId(id);
 		vo.setName(name);
-		vo.setAddress(adr);
+		vo.setAddress(totaladr);
 		vo.setEmail(email);
 		vo.setPassword(pwd);
 		
